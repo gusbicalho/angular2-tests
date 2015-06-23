1d31f51950e45e0a1511d32a3fba659edd2e8e19
Gustavo Bicalho <gusbicalho@gmail.com>
2015-06-23 19:39:40 -0300

    Clean up, leave just basic template

--- app/init.ts
+++ app/init.ts
@@ -4,5 +4,4 @@ System.config({
 });
 
 System.import('./app')
-  .catch(e => console.error(e,
-    'Report this error at https://github.com/mgechev/angular2-seed/issues'));
+  .catch(e => console.error(e.stack));
